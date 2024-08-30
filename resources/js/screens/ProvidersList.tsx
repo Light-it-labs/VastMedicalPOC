782ef19c4188d4db6c7ebea57015361cb5bf845e
joako-gaona <joaquin.gaona@lightit.io>
2024-08-30 15:52:12 -0300

    Bugfix/no task/providers medicare list (#13)
    * feat: change logo
    
    * fix: add figma providers. change rx to pcn
    
    * fix: providers data

--- resources/js/screens/ProvidersList.tsx
+++ resources/js/screens/ProvidersList.tsx
@@ -18,28 +18,76 @@ const medicareDmeProviders: Record<
 > = {
   medicare: [
     {
+      id: 1,
+      name: "Quest Health Solutions",
+      email: "info@questhealthsolutions.com",
+      phoneNumber: "877-270-7050",
+    },
+    {
       id: 2,
-      name: "CCS Medical",
-      email: "help@ccsmed.com ",
-      phoneNumber: "800-599-7521",
+      name: "United States Medical Supplies",
+      email: "abbott@usmed.com",
+      phoneNumber: "877-270-6508",
     },
     {
       id: 3,
+      name: "Better Living Now",
+      email: "libre@betterlivingnow.com",
+      phoneNumber: "800-854-5729",
+    },
+    {
+      id: 4,
       name: "Advanced Diabetes Supply",
       email: "libreintakes@northcoastmed.com",
       phoneNumber: "866-976-9110",
     },
     {
-      id: 4,
-      name: "United States Medical Supply",
-      email: "abbott@usmed.com",
-      phoneNumber: "877-814-5459",
-    },
-    {
       id: 5,
       name: "Edgepark Medical Supplies",
-      email: "edgeparkcareteam@edgepark.com",
+      email: "diabetesreferrals@cardinalhealth.com",
       phoneNumber: "844-619-4650",
+    },
+    {
+      id: 6,
+      name: "CCS Medical",
+      email: "libre@ccsmed.com",
+      phoneNumber: "800-951-1725",
+    },
+    {
+      id: 7,
+      name: "Byram Healthcare/Apria",
+      email: "abbottreferrals@byramhealthcare.com",
+      phoneNumber: "800-974-2055",
+    },
+    {
+      id: 8,
+      name: "J&B Medical Supply",
+      email: "providerservices@jandbmedical.com",
+      phoneNumber: "800-737-0045",
+    },
+    {
+      id: 9,
+      name: "Total Medical Supply",
+      email: "cgm@tmscares.com",
+      phoneNumber: "877-670-1120",
+    },
+    {
+      id: 10,
+      name: "US HealthLink",
+      email: "customerservice@ushealthlink.com",
+      phoneNumber: "855-421-2732",
+    },
+    {
+      id: 11,
+      name: "Bridgewater Health Supplies",
+      email: "info@bridgewaterhealthsupplies.com",
+      phoneNumber: "800-974-2055",
+    },
+    {
+      id: 12,
+      name: "Mini Pharmacy",
+      email: "info@minipharmacy.com",
+      phoneNumber: "888-670-1120",
     },
   ],
   ["private-insurance"]: [
@@ -104,7 +152,7 @@ export const ProvidersList = () => {
   const insuranceKey =
     multiStepFormData?.insuranceFormData?.insuranceType ?? "medicare";
   return (
-    <div className="mx-auto flex w-11/12 flex-col gap-12">
+    <div className="mx-auto flex w-11/12 flex-col gap-12 pb-16">
       <div className="flex flex-col gap-12 text-center">
         <h1 className="pt-8  text-3xl text-[#07284A]">
           Your <b className="text-[#0C8FEB]">Freestyle Libre CGM System</b> is
